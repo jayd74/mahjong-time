b15e221fce05e19f075fc5751b5c3c79f6d92146
Victoria Wong <victoria.ys.wong@gmail.com>
2024-12-31 18:10:03 -0500

    add scoring object

---
+++ src/constants/index.tsx
@@ -0,0 +1,44 @@
+const scores = {
+  3: {
+    'Opponent loss': 32,
+    'Self draw': 16,
+    'Cover all': 48,
+  },
+  4: {
+    'Opponent loss': 64,
+    'Self draw': 32,
+    'Cover all': 96,
+  },
+  5: {
+    'Opponent loss': 96,
+    'Self draw': 48,
+    'Cover all': 144,
+  },
+  6: {
+    'Opponent loss': 128,
+    'Self draw': 64,
+    'Cover all': 192,
+  },
+  7: {
+    'Opponent loss': 192,
+    'Self draw': 96,
+    'Cover all': 288,
+  },
+  8: {
+    'Opponent loss': 256,
+    'Self draw': 128,
+    'Cover all': 288,
+  },
+  9: {
+    'Opponent loss': 384,
+    'Self draw': 192,
+    'Cover all': 384,
+  },
+  10: {
+    'Opponent loss': 512,
+    'Self draw': 256,
+    'Cover all': 768,
+  },
+};
+
+export default scores;
