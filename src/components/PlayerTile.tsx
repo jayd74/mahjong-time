7e154193bf1e5f7d078f11294c6624fd17d3ab03
Victoria Wong <victoria.ys.wong@gmail.com>
2024-12-30 19:13:15 -0500

    fix comment changes

--- src/components/PlayerTile.tsx
+++ src/components/PlayerTile.tsx
@@ -3,48 +3,46 @@ import AddIcon from '@mui/icons-material/Add';
 
 const PlayerTile = () => {
   return (
-    <Box>
-      <Button variant="contained" color="secondary">
-        <Box
+    <Button variant="contained" color="secondary">
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 10,
+          right: 10,
+        }}
+      >
+        <AddIcon
           sx={{
-            position: 'absolute',
-            top: 10,
-            right: 10,
+            width: '85%',
           }}
-        >
-          <AddIcon
-            sx={{
-              width: '85%',
-            }}
-          />
-        </Box>
+        />
+      </Box>
 
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography
+          variant="h5"
+          sx={(theme) => ({
+            color: theme.palette.primary.light,
+          })}
         >
-          <Typography
-            variant="h5"
-            sx={(theme) => ({
-              color: theme.palette.primary.light,
-            })}
-          >
-            Victoria
-          </Typography>
-          <Typography
-            variant="h3"
-            sx={(theme) => ({
-              color: theme.palette.success.main,
-            })}
-          >
-            192
-          </Typography>
-        </Box>
-      </Button>
-    </Box>
+          Victoria
+        </Typography>
+        <Typography
+          variant="h3"
+          sx={(theme) => ({
+            color: theme.palette.success.main,
+          })}
+        >
+          192
+        </Typography>
+      </Box>
+    </Button>
   );
 };
 
